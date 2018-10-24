21f068703e26ae492e6f61cf75ce0ba3fc22f441
krishna <kkrajus777@gmail.com>
2018-10-24 17:32:21 -0500

    feat(ReactBoilerPlate): used webpack 4 for creating simple boilerplate react https://www.valentinog.com/blog/webpack-tutorial/

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <p>React here!</p>
+        <div>check this out</div>
+    </div>
+  )
+}
+
+export default App
+
+ReactDOM.render(<App />, document.getElementById('app'))
