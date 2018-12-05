7a7ad41ec51ce9518f15d8ff0c5b4021b29f8e7c
krishna <kkrajus777@gmail.com>
2018-12-05 11:34:19 -0600

    feat(App): router redux and redux router installed used concurrently and nodemon to run two commands simantaineously

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import {connect} from 'react-redux'
 
-const App = () => {
-  return (
-      <div>chek thos</div>
-  )
+class App extends Component {
+  render () {
+    return (
+      <div className={'container-fluid'}>
+        <div className={'row'}>
+          <div className={'col bg-success'}>check this</div>
+          <div className={'col bg-warning'}>out!!!</div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App
+export default connect(null, null)(App)
 
 ReactDOM.render(<App />, document.getElementById('app'))
