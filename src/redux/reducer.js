58738b9640c6b9bca9812e222fc33256b0765216
krishna <kkrajus777@gmail.com>
2018-12-05 21:53:19 -0600

    feat(redux/utils):both of the important folders added

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -8,14 +8,8 @@ import http from './http/redux'
 import {FormsReducer} from '../utils/form_utils'
 import {fromJS} from 'immutable'
 
-import Dashboard from '../../src/modules/Dashboard/redux/reducer'
-import AppReducer from '../../src/app/redux/reducer'
-import Questionnaire from '../../src/modules/Onboarding/redux/reducer'
-import UsersReducer from '../app/auth/redux/reducer'
-import About from '../../src/modules/About/redux/reducer'
-import Documentation from '../../src/modules/Documentation/redux/reducer'
-import HealthMetrics from '../../src/modules/HealthMetrics/redux/reducer'
-import SuccessStories from '../../src/modules/SuccessStories/redux/reducers'
+// import Dashboard from '../../src/modules/Dashboard/redux/reducer'
+
 
 const initialState = fromJS({
   locationBeforeTransitions: null
@@ -32,13 +26,6 @@ function Routes (state = initialState, {type, payload} = {}) {
 export default combineReducers({
   Routes,
   [http.key]: http,
-  Forms: FormsReducer,
-  [UsersReducer.key]: UsersReducer,
-  [AppReducer.key]: AppReducer,
-  [Dashboard.key]: Dashboard,
-  [Questionnaire.key]: Questionnaire,
-  [About.key]: About,
-  [HealthMetrics.key]: HealthMetrics,
-  [Documentation.key]: Documentation,
-  [SuccessStories.key]: SuccessStories
+    Forms: FormsReducer,
+    // [Dashboard.key]: Dashboard,
 })
