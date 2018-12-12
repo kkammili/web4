1fb9d0a80b2219a8df9ea18e1a15f695b413866d
krishna <kkrajus777@gmail.com>
2018-12-11 21:31:53 -0600

    feat(GitDisplay): arrow functions added pretty important git display done

--- src/Git/components/GitDisplay.js
+++ src/Git/components/GitDisplay.js
@@ -6,19 +6,83 @@ import {connect} from 'react-redux'
 import {Map} from 'immutable'
 
 export class GitDisplay extends Component {
-  componentDidMount () {
-    const userName = 'Raju10100'
-    this.props.fetchGitHubData(userName)
+  constructor (props) {
+    super(props)
+    this.state = {
+      userName: null
+    }
+  }
+  fetchGitData =() => {
+    this.props.fetchGitHubData(this.state.userName)
   }
 
   render () {
-    // console.log(this.props.gitData.toJS(), '<---- props check over here')
     return (
       <div className={'container-fluid'}>
-        <div className={'row'}>
-          <div className={'col bg-success'}>check this</div>
-          <div className={'col bg-warning'}>out!!!</div>
+        <div className={'row pt-5 h2 justify-content-center text-info font-weight-bold'}>
+            Git Hub User Details Displayer
         </div>
+
+        <div className={'row pt-4 pb-5 justify-content-md-center align-items-center'}>
+          <input
+            placeholder={'Enter git username and hit enter'}
+            className={'col-2'}
+            onChange={(e) => this.setState({userName: e.target.value})}
+          />
+          <div className={'p-1'} />
+          <button
+            className={'col-2 justify-content-center  btn btn-dark'}
+            onClick={this.fetchGitData}
+          >Click Me</button>
+        </div>
+        {
+          this.props.gitData.size &&
+              !this.props.gitData.get('name') &&
+          (<div className={'alert alert-danger text-center'}>User name not present</div>)
+        }
+
+        {
+          this.props.gitData.size > 0 &&
+          this.props.gitData.get('name') && (
+            <div style={{border: '2px solid black'}}>
+              <div className={'row justify-content-center pt-4'}>
+                <img
+                  style={{width: '50px', height: '200px', marginRight: '200px'}}
+                  src={this.props.gitData.get('avatar_url')}
+                  className={'col-2 font-weight-bold'}
+                />
+              </div>
+
+              <div className={'row justify-content-center pt-4'}>
+                <div className={'col-2 font-weight-bold'}>Name:</div>
+                <div className={'col-2 font-weight-bold'}>{this.props.gitData.get('name')}</div>
+              </div>
+              <div className={'row justify-content-center pt-4'}>
+                <div className={'col-2 font-weight-bold'}>Company:</div>
+                <div className={'col-2 font-weight-bold'}>{this.props.gitData.get('company')}</div>
+              </div>
+              {
+                this.props.gitData.get('bio') && (
+                  <div className={'row justify-content-center pt-4'}>
+                    <div className={'col-2 font-weight-bold'}>Bio:</div>
+                    <div className={'col-2 font-weight-bold'}>{this.props.gitData.get('bio')}</div>
+                  </div>
+                )
+              }
+
+              {
+                this.props.gitData.get('email') && (
+                  <div className={'row justify-content-center pt-4'}>
+                    <div className={'col-2 font-weight-bold'}>Email:</div>
+                    <div className={'col-2 font-weight-bold'}>{this.props.gitData.get('email')}</div>
+                  </div>
+                )
+              }
+
+            </div>
+          )
+        }
+
       </div>
     )
   }
