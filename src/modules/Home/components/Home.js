8cd1ccf7fe3298562ae1f08c5a8bc130b2fa70e7
krishna <kkrajus777@gmail.com>
2018-12-13 17:12:49 -0600

    removed xml file and added basic styling home page

--- src/modules/Home/components/Home.js
+++ src/modules/Home/components/Home.js
@@ -4,10 +4,15 @@ import {connect} from 'react-redux'
 export class Home extends Component {
   render () {
     return (
-      <div className={'container'}>
+      <div style={{height: '1000px'}} className={'container-fluid bg-dark'}>
         <div className={'row'}>
-          <div className={'col'}>
-                  Hit /git for redirecting to git fetcher project
+          <div className={'col pt-4 pl-4 h4 font-weight-bold text-info'}>
+                  Hit /git route for redirecting to git fetcher project
+          </div>
+        </div>
+        <div className={'row'}>
+          <div className={'col pt-4 pl-4 h4 font-weight-bold text-danger'}>
+                  other projects coming soon
           </div>
         </div>
       </div>
