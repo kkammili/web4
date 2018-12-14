82644119142a24cf4f2db96341e6d1adfe5a1485
krishna <kkrajus777@gmail.com>
2018-12-14 16:12:26 -0600

    feat(user, project): user and project schema created

---
+++ server/routes/project.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+// PROJECT ROUTES
+// router.get('/project/new', project.create) // Create new
+// router.post('/project/new', project.doCreate) // project form
+// router.get('/project/:id', project.displayInfo) // Display project
+// // info
+// router.get('/project/edit/:id', project.edit) // Edit selected
+// // project form
+// router.post('/project/edit/:id', project.doEdit)// Edit selected
+// // project action
+// router.get('/project/delete/:id', project.confirmDelete)// Delete
+// // selected product form
+// router.post('/project/delete/:id', project.doDelete) // Delete
+// // selected project action
+
+module.exports = router
---
+++ server/routes/user.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+
+/* GET users listing. */
+// router.get('/', routes.index);
+// // USER ROUTES
+// router.get('/user', user.index);          // Current user profile
+// router.get('/user/new', user.create);     // Create new user form
+// router.post('/user/new', user.doCreate);  // Create new user action
+// router.get('/user/edit', user.edit);      // Edit current user form
+// router.post('/user/edit', user.doEdit);   // Edit current user action
+// router.get('/user/delete', user.confirmDelete); // delete current
+// //user form
+// router.post('/user/delete', user.doDelete);     // Delete current
+// router.get('/login', user.login);
+// router.post('/login', user.doLogin);
+// router.get('/logout', user.doLogout);
+
+
+//user action
+// Login form
+// Login action
+// Logout current user
+
+module.exports = router
